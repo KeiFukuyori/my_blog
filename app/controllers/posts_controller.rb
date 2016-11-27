e8e5896d9776da858773b2e3b95c31d37d649742
Kei Fukuyori <ocvxkeikei@gmail.com>
2016-11-27 09:50:33 +0000

    リファクタリング(@post = Post.find(params[:id]をbefore_actionでまとめる)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
+  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
     @posts = Post.all
     @new_posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -21,17 +23,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to "/posts/#{@post.id}"
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/posts"
   end
@@ -42,5 +41,8 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body, :category)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
   
 end
